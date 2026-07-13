0ef4cdd276a439ddff2a747002c62f3aa47db376
Vic Genin <aie0.com@gmail.com>
2026-07-14 02:18:30 +0300

    fix: gate Linux-only networking on FD_HAS_LINUX

--- src/disco/net/Local.mk
+++ src/disco/net/Local.mk
@@ -1,6 +1,8 @@
 ifdef FD_HAS_HOSTED
 $(call add-hdrs,fd_net_tile.h)
+ifdef FD_HAS_LINUX
 $(call add-objs,fd_net_tile_topo,fd_disco)
+endif
 endif
 ifdef FD_HAS_LINUX
 $(call add-objs,fd_linux_bond,fd_disco)
