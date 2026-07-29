001595b812075b258145b73cbd36627df2355a5c
Vic Genin <aie0.com@gmail.com>
2026-07-29 11:22:11 +0300

    fix: stub bundle tile on windows build lane

--- src/disco/bundle/Local.mk
+++ src/disco/bundle/Local.mk
@@ -5,6 +5,9 @@ $(call run-unit-test,test_bundle_crank)
 
 $(call add-hdrs,fd_bundle_tile.h)
 ifdef FD_HAS_HOSTED
+ifdef FD_HAS_WINDOWS
+$(call add-objs,fd_bundle_windows_stub,fd_disco)
+else
 $(call add-objs,fd_bundle_auth fd_bundle_client,fd_disco)
 $(call make-unit-test,test_bundle_client,test_bundle_client,fd_disco fd_waltz fd_flamenco fd_tango fd_ballet fd_util,$(OPENSSL_LIBS))
 $(call run-unit-test,test_bundle_client)
@@ -17,9 +20,14 @@ $(call make-unit-test,test_bundle_tile,test_bundle_tile,fd_disco fd_waltz fd_fla
 $(call run-unit-test,test_bundle_tile)
 endif
 endif
+endif
 
 ifdef FD_HAS_HOSTED
+ifdef FD_HAS_WINDOWS
+# fd_bundle_windows_stub already provides fd_tile_bundle for Windows build lanes.
+else
 ifdef FD_HAS_DOUBLE
 $(call add-objs,fd_bundle_tile,fd_disco)
+endif
 endif
 endif
---
+++ src/disco/bundle/fd_bundle_windows_stub.c
@@ -0,0 +1,71 @@
+#include "fd_bundle_tile.h"
+
+#if FD_HAS_WINDOWS
+
+FD_FN_CONST static ulong
+scratch_align( void ) {
+  return 1UL;
+}
+
+FD_FN_CONST static ulong
+scratch_footprint( fd_topo_tile_t const * tile ) {
+  (void)tile;
+  return 0UL;
+}
+
+FD_FN_CONST static ulong
+loose_footprint( fd_topo_tile_t const * tile ) {
+  (void)tile;
+  return 0UL;
+}
+
+static ulong
+populate_allowed_seccomp( fd_topo_t const * topo,
+                          fd_topo_tile_t const * tile,
+                          ulong out_cnt,
+                          struct sock_filter * out ) {
+  (void)topo; (void)tile; (void)out_cnt; (void)out;
+  return 0UL;
+}
+
+static ulong
+populate_allowed_fds( fd_topo_t const * topo,
+                      fd_topo_tile_t const * tile,
+                      ulong out_fds_cnt,
+                      int * out_fds ) {
+  (void)topo; (void)tile; (void)out_fds_cnt; (void)out_fds;
+  return 0UL;
+}
+
+static void
+privileged_init( fd_topo_t const * topo,
+                 fd_topo_tile_t const * tile ) {
+  (void)topo; (void)tile;
+}
+
+static void
+unprivileged_init( fd_topo_t const * topo,
+                   fd_topo_tile_t const * tile ) {
+  (void)topo; (void)tile;
+}
+
+static void
+run( fd_topo_t * topo,
+     fd_topo_tile_t * tile ) {
+  (void)topo; (void)tile;
+  FD_LOG_ERR(( "bundle tile unsupported on Windows build lane" ));
+}
+
+fd_topo_run_tile_t fd_tile_bundle = {
+  .name                     = "bundle",
+  .populate_allowed_seccomp = populate_allowed_seccomp,
+  .populate_allowed_fds     = populate_allowed_fds,
+  .scratch_align            = scratch_align,
+  .scratch_footprint        = scratch_footprint,
+  .loose_footprint          = loose_footprint,
+  .privileged_init          = privileged_init,
+  .unprivileged_init        = unprivileged_init,
+  .run                      = run,
+};
+
+#endif
